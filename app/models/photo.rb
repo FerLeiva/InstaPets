0f073ea645e4e44ea1dcecbe173131e6ac84f5c2
FerLeiva <efeleiva@gmail.com>
2022-03-09 15:04:47 -0300

    Minor updates

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,3 @@
 class Photo < ApplicationRecord
+	enum license: [:copyright, :copyleft, :creative_commons]
 end
--- db/migrate/20220309170301_create_photos.rb
+++ db/migrate/20220309170301_create_photos.rb
@@ -1,10 +1,10 @@
 class CreatePhotos < ActiveRecord::Migration[7.0]
   def change
     create_table :photos do |t|
-      t.string :name
+      t.string :name, limit: 150
       t.string :url
-      t.text :description
-      t.integer :license
+      t.text :description, null: false, default: ''
+      t.integer :license, default: 0
 
       t.timestamps
     end
