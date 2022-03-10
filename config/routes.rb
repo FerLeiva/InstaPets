e077b33aa48c4f66c6ed608f75364e318a17599e
FerLeiva <efeleiva@gmail.com>
2022-03-09 21:10:29 -0300

    Log in - out

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   # Welcom Controller
   get 'welcome/index', to: 'welcome#index', as: 'welcome_home'
 
+  # Users Controller
+  get 'login', to: 'users#login', as: 'login'
+  post 'login', to:'users#new_session', as:'user_new_session'
+  get 'logout', to: 'users#logout', as: 'logout'
+
   #Root
   root to: 'welcome#index'
 
